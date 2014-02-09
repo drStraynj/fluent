82375b4245918c6c6b426c0ad5c7dc127d73edca
drStraynj <i.left.my.towel@gmail.com>
2014-02-09 23:08:53 +0000

    Started array mods

--- fluent.js
+++ fluent.js
@@ -1,4 +1,4 @@
-(function fluent(f) {
+(function(f) {
   f.map = function() {
     var a = arguments, f = this;
     return function() {
@@ -19,15 +19,25 @@
     var a = arguments, f = this;
     return function() {
       var r = f.apply(this, arguments);
-      for (var i=0; i<a.length; i++) 
+      for (var i=0; i<a.length; i++)
         if (typeof a[i] == "function")
           r = (r || r == 0 || r == '') && r + a[i].apply(this, arguments);
         else r = (r || r == 0 || r == '') && r + a[i];
       return r;
     };
   };
-  
   function push(obj, args) {
     return obj.push.apply(obj, args) >= 0 && obj;
   };
 })(Function.prototype);
+(function(a) {
+ a.method = function(name) {
+   var args = Array.prototype.slice.call(arguments, 1);
+   return args.length
+     ? this.map(function(d) { return d[name] && d[name].apply(d, args); })
+     : this.map(function(d) { return d[name] && d[name](); });
+ };
+ a.merge = function() {
+   return this.reduce(function(a, b) { return (b && a.concat(b)) || a; });
+ };
+})(Array.prototype);
